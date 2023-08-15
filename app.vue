532d0e021e2d298ec765889b7d202829a0e0df78
lapinRagnar <lapinragnar@gmail.com>
2023-08-15 17:19:28 +0200

    4- CreatePost overlay | nuxt component

--- app.vue
+++ app.vue
@@ -4,5 +4,18 @@
     
     
     </NuxtPage>
+
+    <CreatePost
+      :class="[
+        {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
+        {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
+      ]"
+    />
+
   </div>
 </template>
+
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+</script>
